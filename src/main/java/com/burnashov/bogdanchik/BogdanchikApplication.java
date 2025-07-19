1370a5bda7bc724596a4606cd79284377386bd3b
Poptsov Roman <poptsov.roman@mail.ru>
2025-07-19 23:41:03 +0500

    extract keyword services, clean MyBot logic

--- src/main/java/com/burnashov/bogdanchik/BogdanchikApplication.java
+++ src/main/java/com/burnashov/bogdanchik/BogdanchikApplication.java
@@ -19,7 +19,7 @@ public class BogdanchikApplication {
 
         try {
             botsApi.registerBot(bot);
-            System.out.println("Bot registered successfully");
+            System.out.println("Bot registered successfully. Version 0.0.2");
         } catch (TelegramApiException e) {
             e.printStackTrace();
             log.info(e.getMessage());
